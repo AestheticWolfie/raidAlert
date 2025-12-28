fa9304625ff78749846b13299a526e831a0dd997
Drake Wolstenholme <drakewolstenholme22@gmail.com>
2025-12-28 15:08:33 +0000

    Changed a message in ready
    I just want to test workflows and make PR

--- src/events/clientReady/0ready.js
+++ src/events/clientReady/0ready.js
@@ -2,5 +2,5 @@
  * @param {import('discord.js').Client} client
  */
 export default (client) => {
-  console.log(`🤖 ${client.user.tag} is online.`);
+  console.log(`🤖 ${client.user.tag} is online and ready.`);
 };
