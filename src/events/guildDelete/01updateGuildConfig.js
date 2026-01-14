a21cde911c1dfde725dfa1fb59052ed0da5b3115
Drake Wolstenholme <drakewolstenholme22@gmail.com>
2026-01-14 15:28:31 +0000

    PostSchedule Fixes
    - Fixed guild being kicked and readded makes a new document in db
    - Fixed text in embed not grammatically making sense

---
+++ src/events/guildDelete/01updateGuildConfig.js
@@ -0,0 +1,44 @@
+import GuildConfig from "../../models/guildConfig.js";
+
+import { createErrorNotifier } from "../../utils/errorHandler.js";
+
+/**
+ * @param {import('discord.js').Guild} guild
+ * @param {import('discord.js').Client} client
+ *
+ * @description Updates the guildConfig that the guild has been kicked using the isKicked field
+ */
+export default async (guild, client) => {
+  let currentGuildDocument;
+  try {
+    currentGuildDocument = await GuildConfig.findOne({ guildId: guild.id });
+  } catch (error) {
+    await createErrorNotifier(
+      client,
+      NOTIFICATION_DEV_CHANNEL,
+      DRAKE_DEV_ID,
+      "Fetching GuildConfig object",
+      error
+    );
+    return;
+  }
+
+  if (currentGuildDocument === null) {
+    return;
+  }
+
+  currentGuildDocument.isKicked = true;
+
+  try {
+    await currentGuildDocument.save();
+  } catch (error) {
+    await createErrorNotifier(
+      client,
+      NOTIFICATION_DEV_CHANNEL,
+      DRAKE_DEV_ID,
+      "Saving GuildConfig object",
+      error
+    );
+    return;
+  }
+};
